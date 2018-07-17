aa21c55dcb8424ee82f6cd24f60c70ce491f0eeb
Theo Dedeken <theo.dedeken@ugent.be>
2018-07-18 00:37:20 +0200

    Ast structure

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,51 @@
 use pest::iterators::Pair;
 
+pub enum Value {
+    True,
+    False,
+    Zero,
+}
+
+pub enum Operator {
+    Succ,
+    Pred,
+}
+
+pub enum Type {
+    Bool,
+    Nat,
+}
+
+pub enum ASTNode {
+    AbstractionNode{
+        ident: Box<ASTNode>,
+        data_type: Type,
+        body: Box<ASTNode>,        
+    },
+    ApplicationNode{
+        left: Box<ASTNode>, 
+        right: Box<ASTNode>,
+    },
+    IdentifierNode{
+        name: String
+    },
+    ConditionNode{
+        clause: Box<ASTNode>,
+        then_arm: Box<ASTNode>, 
+        else_arm: Box<ASTNode>
+    },
+    ArithmeticNode{
+        op: Operator, 
+        expr: Box<ASTNode>
+    },
+    IsZeroNode{
+        expr: Box<ASTNode>
+    },
+    ValueNode{
+        value: Value
+    },
+}
+
+
+
 pub fn build_ast() {}
